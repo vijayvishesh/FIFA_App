ae8d4d874da895023930692765fb69fc90bf5d6e
vijay yadav <vijayvishesh18@gmail.com>
2017-12-24 11:46:29 +0530

    Push source in master

---
+++ FIFA_App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FIFA_App
+//
+//  Created by apple on 12/24/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FIFA_App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FIFA_App
+//
+//  Created by apple on 12/24/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
